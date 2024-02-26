ad9c99d2d93fea3d80cb1e131ca0678d8ecdc880
Peter Sideris <petersid2022@gmail.com>
2024-02-26 13:06:08 +0200

    add: start the yank autocommand

--- lua/clippy/init.lua
+++ lua/clippy/init.lua
@@ -1,8 +1,10 @@
 local ui = require('clippy.ui')
+local autocmd = require('clippy.autocmd')
 local M = {}
 
 M.setup = function()
-	vim.keymap.set({ "n", "i", "v" }, "<C-e>", function() ui:toggle_menu() end)
+	vim.keymap.set({ "n", "i", "v" }, "<C-c>", function() ui:toggle_menu() end)
+	autocmd:yank()
 end
 
 return M
