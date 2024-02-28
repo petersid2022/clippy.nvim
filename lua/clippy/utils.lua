10ca608e5161dfceb349870983f7f15003763cbe
Peter Sideris <petersid2022@gmail.com>
2024-02-28 16:28:11 +0200

    add: reverse array function

--- lua/clippy/utils.lua
+++ lua/clippy/utils.lua
@@ -6,4 +6,14 @@ function M.buffer_to_string(buffer_id)
     return vim.api.nvim_buf_get_lines(buffer_id, 0, vim.api.nvim_buf_line_count(buffer_id), false)
 end
 
+---@param arr string[]
+---@return string[]
+function M.reverse_array(arr)
+    local reversedArray = {}
+    for i = 1, #arr do
+        reversedArray[i] = arr[#arr - i + 1]
+    end
+    return reversedArray
+end
+
 return M
